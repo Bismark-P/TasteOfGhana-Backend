39dd62320888d9cbdc0f2a56867f34a39acfee27
Bismark-P <bismarkpinkrah@hotmail.com>
2025-08-11 10:44:44 +0000

    admin schema updated for role

--- Schemas/adminSchema.js
+++ Schemas/adminSchema.js
@@ -18,6 +18,11 @@ const adminSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  role: {
+    type: String,
+    enum: ['admin'],
+    default: 'admin'
+  },
   createdAt: {
     type: Date,
     default: Date.now
